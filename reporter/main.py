80f6757d97ed2b3b8acfc9c20fb42e979b6f2c19
HUSSEIN Adam <mh54500@gmail.com>
2023-01-26 00:56:15 +0100

    Changes on templates with css variable

--- reporter/main.py
+++ reporter/main.py
@@ -1,6 +1,18 @@
 import report_filler
 
+# from jinja2 import Environment, FileSystemLoader
+import jinja2 as jinja
+
 
 filler = report_filler.ReportFiller()
 json = filler.get_json_data("data_samples/input.json")
+environment = jinja.Environment(loader=jinja.FileSystemLoader("reporter/templates/"))
+template = environment.get_template("species_template.html")
 dictionnaire = filler.get_species_data(json)
+
+filename = "species_report.html"
+# results_template = environment.get_template(filename)
+context = {"species_data": dictionnaire}
+
+with open(filename, mode="w", encoding="utf-8") as results:
+    results.write(template.render(context))
--- reporter/report_filler.py
+++ reporter/report_filler.py
@@ -3,24 +3,40 @@ import jinja2 as jinja
 import json
 from pandas import json_normalize
 
+
 class ReportFiller:
+    """def get_template(self, filename):
+    environment = jinja.Environment(loader=jinja.FileSystemLoader("reporter/templates/"))
+    template = environment.get_template(filename)
+    return template
+    """
 
-    def get_template(self,filename):
-        environment = jinja.Environment(loader=jinja.FileSystemLoader("templates/"))
-        template = environment.get_template(filename)
-        return template
-
-    def get_json_data(self,filename):
+    def get_json_data(self, filename):
         data = json.load(open(filename))
         df = json_normalize(data)
         return df
-        
-    def get_species_data(self,df):
+
+    def get_species_data(self, df):
         dict = {}
-        dict['species_name'] = df['species_name.content.name'][0]
-        ref_desc = ""
-        for ref in df['description.references'][0]:
-            ref_desc += df[ref+".content"][0]
-        dict['description'] = ref_desc
-        #dict['description'] = df['description.content'][0]
+        content = ""
+        content = df["species_name.content.name"][0]
+        dict["species_name"] = content
+        for ref in df["species_name.references"][0]:
+            content = df[ref + ".content"][0]
+            dict["species_name"] = content
+
+        content = df["description.content"][0]
+        dict["description"] = content
+        for ref in df["description.references"][0]:
+            content = df[ref + ".content"][0]
+            dict["description"] = content
+
+        dict["conservation_status"] = df["conservation_status"][0]
+        dict["current_habitats"] = df["current_habitats"][0]
+        dict["lost_habitats"] = df["lost_habitats"][0]
+        dict["threats"] = df["threats"][0]
+        dict["conservation_measures"] = df["conservation_measures"][0]
+        dict["references"] = df["references"][0]
+
+        # dict['description'] = df['description.content'][0]
         return dict
