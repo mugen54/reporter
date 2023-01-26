8c4dd3dc6b561b2e9d5ffe058f7308158138e606
HUSSEIN Adam <mh54500@gmail.com>
2023-01-26 01:30:28 +0100

    Resolve conflict in report_filler file

--- reporter/report_filler.py
+++ reporter/report_filler.py
@@ -5,20 +5,12 @@ from pandas import json_normalize
 
 
 class ReportFiller:
-<<<<<<< HEAD
     def get_template(self, filename):
         environment = jinja.Environment(
             loader=jinja.FileSystemLoader("reporter/templates/")
         )
         template = environment.get_template(filename)
         return template
-=======
-    """def get_template(self, filename):
-    environment = jinja.Environment(loader=jinja.FileSystemLoader("reporter/templates/"))
-    template = environment.get_template(filename)
-    return template
-    """
->>>>>>> templates_with_css
 
     def get_json_data(self, filename):
         data = json.load(open(filename))
@@ -28,7 +20,6 @@ class ReportFiller:
     def get_species_data(self, df):
         dict = {}
         content = ""
-<<<<<<< HEAD
         # species_name
         content = df["species_name.content.name"][0]
         for ref in df["species_name.references"][0]:
@@ -53,28 +44,6 @@ class ReportFiller:
         dict["conservation_measures"] = df["conservation_measures"][0]
         # references
         dict["references"] = df["references"][0]
-=======
-        content = df["species_name.content.name"][0]
-        dict["species_name"] = content
-        for ref in df["species_name.references"][0]:
-            content = df[ref + ".content"][0]
-            dict["species_name"] = content
-
-        content = df["description.content"][0]
-        dict["description"] = content
-        for ref in df["description.references"][0]:
-            content = df[ref + ".content"][0]
-            dict["description"] = content
-
-        dict["conservation_status"] = df["conservation_status"][0]
-        dict["current_habitats"] = df["current_habitats"][0]
-        dict["lost_habitats"] = df["lost_habitats"][0]
-        dict["threats"] = df["threats"][0]
-        dict["conservation_measures"] = df["conservation_measures"][0]
-        dict["references"] = df["references"][0]
-
-        # dict['description'] = df['description.content'][0]
->>>>>>> templates_with_css
         return dict
 
     def get_string_from_file(self, file):
