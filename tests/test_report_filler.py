c1a3dfcac692538e6cf6385d95d04a0a0a9687f4
HUSSEIN Adam <mh54500@gmail.com>
2023-02-13 14:32:49 +0100

    Test implementation

---
+++ tests/test_report_filler.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+from unittest import TestCase, main
+
+from reporter.report_filler import ReportFiller
+
+
+class ReportFillerTestsCases(TestCase):
+    def test_get_list(self):
+        dataframe = pd.DataFrame(
+            {
+                "current_habitats.content": {
+                    "name": "Serengenti National Reserve",
+                    "population_size": 500,
+                    "habitat_quality": "Good",
+                }
+            }
+        )
+        key = "current_habitats"
+        report_filler = ReportFiller()
+        list = report_filler.get_list(df=dataframe, key=key)
+        attended_return = [
+            {
+                "current_habitats.content": {
+                    "name": "Serengenti National Reserve",
+                    "population_size": 500,
+                    "habitat_quality": "Good",
+                }
+            }
+        ]
+        self.assertTrue(
+            list == attended_return,
+            " Error, the returned list does not match the pattern.",
+        )
+
+
+if __name__ == "__main__":
+    main()
